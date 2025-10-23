f06ba51566a67850ae64ee0ed3adb09583ae5a78
swetha-dhanasekran <dhanasekaranswetha8@gmail.com>
2025-10-23 16:14:08 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
+public class main {
+    public static void main(String[] args) {
+        LinkedHashSet<String>set=new LinkedHashSet<>();
+        names.add("sakthi");
+        names.add("varsha");
+        names.add("swetha";
+        System.out.println(names);
+        Iterator<String>it=names.Iterator();
+        while (it.hashNext()) {
+            System.out.println(it.next());
+            
+        }
+
+        
+    }
+    
+}
